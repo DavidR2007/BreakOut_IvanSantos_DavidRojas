ae8d4274b03825321581ac2f6f1f9133e1cbc215
Ivan Santos Polo <ivan.santos@estudiant.enti.cat>
2026-03-27 13:10:55 +0100

    scene manager done

--- Project1/GameScene.h
+++ Project1/GameScene.h
@@ -1,28 +1,68 @@
 #pragma once
 #include "Scene.h"
 
-class Gameplay : public Scene {
+class GameplayScene : public Scene {
 
 
 public:
-	void Start()
-	{
-		std::cout << "Estoy empezando gameplay";
-	}
+    void CreateWalls() {
+        for (int i = 0; i < MAP_SIZE; i++) {
+            objects.push_back(new Wall(Vector2(i, 0), YELLOW, false));
+            objects.push_back(new Wall(Vector2(i, MAP_SIZE - 1), YELLOW, true));
 
+            if (i > 0 && i < MAP_SIZE - 1) {
+                objects.push_back(new Wall(Vector2(0, i), YELLOW, false));
+                objects.push_back(new Wall(Vector2(MAP_SIZE - 1, i), YELLOW, false));
+            }
+        }
+    }
 
-	void Update()
-	{
-		while (true)
-		{
-			Sleep(100);
-			std::cout << "Estoy en Gameplay";
-		}
-		
-	}
+    void CreateBricks() {
+        for (int row = 1; row <= 3; row++) {
+            for (int col = 1; col < MAP_SIZE - 1; col++) {
+                objects.push_back(new Brick(Vector2(col, row), CYAN));
+            }
+        }
+    }
 
-	void Render() override
-	{
+    void CreatePlayer() {
+        objects.push_back(new Pad(Vector2(MAP_SIZE / 2, (MAP_SIZE * 3) / 4), WHITE, 1, MAP_SIZE));
+    }
 
-	}
+    void CreateBall() {
+        objects.push_back(new Ball(Vector2(2, MAP_SIZE / 2), WHITE, objects));
+    }
+
+    void Start() override{
+        CreateWalls();
+        CreateBricks();
+        CreatePlayer();
+        CreateBall();
+    }
+
+    void Render() override{
+        system("cls");
+        ConsoleSetColor(WHITE, BLACK);
+
+        for (int i = 0; i < objects.size(); i++) {
+            objects[i]->Render();
+        }
+
+        ConsoleXY(MAP_SIZE, MAP_SIZE);
+    }
+
+    void Update() override{
+        bool isPlaying = true;
+
+        while (isPlaying) {
+            Sleep(100);
+
+            for (int i = 0; i < objects.size(); i++) {
+                objects[i]->Update();
+            }
+
+            Render();
+        }
+    }
+
 };
--- Project1/Scene.h
+++ Project1/Scene.h
@@ -22,6 +22,6 @@ public:
 
 	SceneIndex ChangeScene()
 	{
-		return;
+		return nextScene;
 	}
 };
